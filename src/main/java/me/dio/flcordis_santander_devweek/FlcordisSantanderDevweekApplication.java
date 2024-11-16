da0325508a46fb80920c519c9b3c62539677b18e
FLCordis <flavio.cordis@hotmail.com>
2024-11-16 17:50:20 -0300

    INIT: Projeto iniciado do Santander Dev Week, e adicionado o diagrama de classes.

---
+++ src/main/java/me/dio/flcordis_santander_devweek/FlcordisSantanderDevweekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.flcordis_santander_devweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlcordisSantanderDevweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlcordisSantanderDevweekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/flcordis_santander_devweek/FlcordisSantanderDevweekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.flcordis_santander_devweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlcordisSantanderDevweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
